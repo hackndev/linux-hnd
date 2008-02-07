92181ae5ab3cecfd6840ac69e96af64fb2528c55
Michal Hrusecky <Michal.Hrusecky@seznam.cz>
2008-02-07 14:04:16 +0100

    Generic Device: Logical key mappings (solving bug #72)

---
+++ include/linux/palm_keys.h
@@ -0,0 +1,30 @@
+#ifndef _PALM_KEYS_H
+#define _PALM_KEYS_H
+
+#include <linux/input.h>
+
+// Ordinary buttons
+#define PALM_KEY_CALENDAR		KEY_F10
+#define PALM_KEY_CONTACTS		KEY_F11
+#define PALM_KEY_FOLDER			KEY_F1
+#define PALM_KEY_HOME			KEY_F9
+#define PALM_KEY_MUSIC			KEY_F2
+#define PALM_KEY_PHOTOS			KEY_F3
+#define PALM_KEY_ROTATE			KEY_F4
+#define PALM_KEY_STAR			KEY_F5
+#define PALM_KEY_TODO			KEY_F6
+#define PALM_KEY_VOICE_MEMO		KEY_F7
+#define PALM_KEY_WEB			KEY_F12
+
+// DPad
+#define PALM_KEY_DPAD_UP		KEY_UP
+#define PALM_KEY_DPAD_DOWN		KEY_DOWN
+#define PALM_KEY_DPAD_LEFT		KEY_LEFT
+#define PALM_KEY_DPAD_RIGHT		KEY_RIGHT
+#define PALM_KEY_DPAD_CENTER		KEY_ENTER
+
+// Special buttons
+#define PALM_KEY_HOTSYNC		KEY_F13
+#define PALM_KEY_POWER			KEY_F8
+
+#endif
